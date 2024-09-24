3fe0ac372ffbb06b570bdb904c8fc41314f34e1f
BaptisteArchambaud <archambaudbaptiste@gmail.com>
2024-09-24 15:19:14 +0200

    Update ggplot_tox_probas
    Add possibility to include credible interval in ggplot_tox_probas output

--- R/utils_functions.R
+++ R/utils_functions.R
@@ -7,13 +7,16 @@
 #'
 #' @examples
 #' @importFrom dplyr bind_rows
-#' @importFrom ggplot2 aes geom_hline geom_line geom_point ggplot labs scale_color_manual scale_y_continuous theme_light
-ggplot_tox_probas <- function(modelfit, target = 0.25){
+#' @importFrom ggplot2 aes geom_hline geom_line geom_point geom_ribbon ggplot labs scale_color_manual scale_fill_manual scale_y_continuous theme_light
+#' @importFrom tibble tibble
+ggplot_tox_probas <- function(modelfit, target = 0.25, CI = F){
 
   #skeleton probabilities
   data_plot <- tibble(dose_level = 1:length(modelfit$skeleton),
                       all_tox_probas = modelfit$skeleton,
-                      proba_id = "Skeleton")
+                      proba_id = "Skeleton",
+                      lower_probas = modelfit$skeleton,
+                      upper_probas = modelfit$skeleton)
 
   #if at least 1 patient included then also add posterior probabilities
   if(nrow(modelfit$df) > 0){
@@ -21,11 +24,13 @@ ggplot_tox_probas <- function(modelfit, target = 0.25){
       bind_rows(
         tibble(dose_level = 1:length(modelfit$skeleton),
                all_tox_probas = modelfit$dfcrm_fit$ptox,
+               lower_probas = modelfit$dfcrm_fit$ptoxL,
+               upper_probas = modelfit$dfcrm_fit$ptoxU,
                proba_id = "Posterior probabilities")
       )
   }
 
-  data_plot %>% ggplot(aes(x = dose_level, y = all_tox_probas, color = proba_id)) +
+  plot <- data_plot %>% ggplot(aes(x = dose_level, y = all_tox_probas, color = proba_id)) +
     scale_color_manual(values = c("Skeleton" = "#F8766D", "Posterior probabilities" = "#619CFF")) +
     geom_point() +
     geom_line() +
@@ -33,6 +38,15 @@ ggplot_tox_probas <- function(modelfit, target = 0.25){
     labs(x = "Dose level", y = "Probability of toxicity", color = "") +
     scale_y_continuous(breaks = seq(0, 1, 0.25), limits = c(0, 1)) +
     theme_light()
+
+  if(CI){
+    plot <- plot +
+      geom_ribbon(aes(ymin = lower_probas, ymax = upper_probas, fill = proba_id),
+                  alpha = 0.3, show.legend = F, colour = NA) +
+      scale_fill_manual(values = c("Skeleton" = "#F8766D", "Posterior probabilities" = "#619CFF"))
+  }
+
+  plot
 
 }
 
